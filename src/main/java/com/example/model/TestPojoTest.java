ff20741984e5a6fbf47eebb74d9feac265cbac90
MoheyElDin Badr <MoheyElDin.Badr@gmail.com>
2023-06-02 02:54:19 +0300

    Initial commit

---
+++ src/main/java/com/example/model/TestPojoTest.java
@@ -0,0 +1,20 @@
+package com.example.model;
+
+import java.util.List;
+
+public class TestPojoTest {
+
+    public TestPojoTest(List<GameOfThrones> game_of_thrones_list) {
+        this.game_of_thrones_list = game_of_thrones_list;
+    }
+
+    private List<GameOfThrones> game_of_thrones_list;
+
+    public List<GameOfThrones> getGame_of_thrones_list() {
+        return game_of_thrones_list;
+    }
+
+    public void setGame_of_thrones_list(List<GameOfThrones> game_of_thrones_list) {
+        this.game_of_thrones_list = game_of_thrones_list;
+    }
+}
